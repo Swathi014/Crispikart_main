ff6492946fa5230b954469c493065722dea4be43
Swathi P <swathisoman2353@gmail.com>
2023-07-16 10:47:35 +0530

    database changed

--- Restorant/settings.py
+++ Restorant/settings.py
@@ -80,23 +80,23 @@ WSGI_APPLICATION = 'Restorant.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.mysql',
-#         'NAME': 'crispikart',
-#         'USER': 'root',
-#         'PASSWORD': '',
-#         'HOST': 'localhost',  # If running locally, use 'localhost'
-#         'PORT': '3306',
-#     }
-# }
-
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'crispikart',
+        'USER': 'root',
+        'PASSWORD': '',
+        'HOST': 'localhost',  # If running locally, use 'localhost'
+        'PORT': '3306',
     }
 }
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 
 
 # Password validation
